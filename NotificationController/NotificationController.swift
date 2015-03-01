bec6d64e6cedf0baf02a96a2e0e4a845bcf97b84
kukushi <17washabi@gmail.com>
2015-03-01 19:44:40 +0800

    initial commit

---
+++ NotificationController/NotificationController.swift
@@ -0,0 +1,9 @@
+//
+//  NotificationController.swift
+//  NotificationController
+//
+//  Created by kukushi on 3/1/15.
+//  Copyright (c) 2015 kukushi. All rights reserved.
+//
+
+import Foundation
